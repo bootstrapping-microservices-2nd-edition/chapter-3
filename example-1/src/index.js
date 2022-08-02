abf9aa3910ab3f814575285c31058c4e90c34ae9
Ashley Davis <ashley@codecapers.com.au>
2022-08-03 07:55:29 +1000

    Updated code to use async/await.

--- example-1/src/index.js
+++ example-1/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
 
 const app = express();
 
@@ -19,22 +18,16 @@ const PORT = process.env.PORT;
 //
 // Registers a HTTP GET route for video streaming.
 //
-app.get("/video", (req, res) => {
+app.get("/video", async (req, res) => { // Route for streaming video.
+    
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    const stats = await fs.promises.stat(videoPath);
 
-    const videoPath = path.join("./videos", "SampleVideo_1280x720_1mb.mp4");
-    fs.stat(videoPath, (err, stats) => {
-        if (err) {
-            console.error("An error occurred ");
-            res.sendStatus(500);
-            return;
-        }
-
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-        fs.createReadStream(videoPath).pipe(res);
+    res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
     });
+    fs.createReadStream(videoPath).pipe(res);
 });
 
 //
